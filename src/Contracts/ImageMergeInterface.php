dedcd9725a96f69ff635d70ca506343abf7ae5a9
Bill li <bill.li@ofcold.com>
2019-02-11 21:52:37 +0800

    contracts

---
+++ src/Contracts/ImageMergeInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ofcold\QrCode\Contracts;
+
+use Ofcold\QrCode\Image;
+
+interface ImageMergeInterface
+{
+	/**
+	 * Creates a new ImageMerge object.
+	 *
+	 * @param $sourceImage Image The image that will be merged over.
+	 * @param $mergeImage Image The image that will be used to merge with $sourceImage
+	 */
+	public function __construct(Image $sourceImage, Image $mergeImage);
+
+	/*
+	 * Returns an QrCode that has been merge with another image.  This is usually used with logos to imprint a logo into a QrCode.
+	 *
+	 * @return string
+	 */
+	public function merge($percentage);
+}
